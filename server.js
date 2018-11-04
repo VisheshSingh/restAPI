1168e0a3f2af84656d696411560ee87ec589bc51
Vishesh <visheshsingh16@gmail.com>
2018-11-04 10:20:57 -0500

    Testing CRUD via rest client

--- server.js
+++ server.js
@@ -8,6 +8,23 @@ app.get("/", (req, res) => {
   res.json({ info: "The users API is available at /users" });
 });
 
+// GET REQUEST
+app.get("/users", (req, res) => {
+  res.json({ info: "GET request..." });
+});
+// POST REQUEST
+app.post("/users", (req, res) => {
+  res.json({ info: "POST request..." });
+});
+// PUT REQUEST
+app.put("/users", (req, res) => {
+  res.json({ info: "PUT request..." });
+});
+// DELETE REQUEST
+app.delete("/users", (req, res) => {
+  res.json({ info: "DELETE request..." });
+});
+
 // listen to port
 app.listen(9090, () => {
   console.log("Server running at port 9090...");
